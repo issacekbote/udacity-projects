77c45b0440d224abf1af2070e5c505bf550dc7d0
Issac Ekbote <issac.ekbote@yahoo.com.au>
2019-08-31 23:52:59 +1000

    feat: Add airport and other codes dimension table

--- CapstoneProject/etl.py
+++ CapstoneProject/etl.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 import datetime
 from pyspark.sql import SparkSession
@@ -5,9 +6,19 @@ from pyspark.sql.functions import udf, col, from_unixtime, \
     monotonically_increasing_id
 from pyspark.sql.functions import year, month, dayofmonth, \
     hour, weekofyear, date_format, dayofweek
-from pyspark.sql.types import TimestampType, DoubleType, IntegerType, LongType
+from pyspark.sql.types import TimestampType, DoubleType, \
+    IntegerType, LongType
 from pyspark.sql import functions as F
+from pyspark.sql.functions import split
 
+#Read config file to get AWS keys
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config\
+            .get('AWS','AWS_ACCESS_KEY_ID')
+os.environ['AWS_SECRET_ACCESS_KEY']=config\
+            .get('AWS','AWS_SECRET_ACCESS_KEY') 
 
 def create_spark_session():
     """
@@ -16,25 +27,33 @@ def create_spark_session():
     spark = SparkSession \
         .builder \
         .config("spark.jars.packages", \
-        "saurfang:spark-sas7bdat:2.0.0-s_2.11", \
-        "org.apache.hadoop:hadoop-aws:2.7.0") \
+        "saurfang:spark-sas7bdat:2.0.0-s_2.11") \
         .appName("sparkimmietl") \
         .getOrCreate()
     return spark
 
 def process_immigration_data(spark, input_data, output_data):
     """
-    This function processes the immigration files to extract immigrants data. 
-    The function transforms the extracted data and
-    it then writes data to s3 as a parquet files. 
+    This function processes the immigration files to 
+    extract immigrants data. The function transforms 
+    the extracted data and it then writes data to s3 as 
+    a parquet files. 
     """
     
     #set input path
-    input_data = input_data + "18-83510-I94-Data-2016/i94_jan16_sub.sas7bdat"
-
+    input_data = input_data + "18-83510-I94-Data-2016/"
+    in_jul = input_data + "i94_jan16_sub.sas7bdat"
+    in_aug = input_data + "i94_jan16_sub.sas7bdat"
+    
     #read immigration data file
-    df=spark.read.format('com.github.saurfang.sas.spark')\
-        .load(input_data)
+    df_jul=spark.read.format('com.github.saurfang.sas.spark')\
+        .load(in_jul)
+    df_aug=spark.read.format('com.github.saurfang.sas.spark')\
+        .load(in_aug)
+    
+    #merge dataframes
+    df = df_jul.union(df_aug)
+
 
     #extract required filed into staging table
     immistaging_table = df.select([monotonically_increasing_id().\
@@ -43,7 +62,11 @@ def process_immigration_data(spark, input_data, output_data):
         "i94addr","depdate", "i94bir", "i94visa",\
         "gender", "visatype"])
 
-    #update depdate column
+    # drop null values in arrdate column to avoid wrong analysis 
+    # and ensure high data quality
+    immistaging_table = immistaging_table.na.drop(subset=["arrdate"])
+    
+    #update depdate column with zero for null values
     immistaging_table = immistaging_table.withColumn('depdate',F.when\
         (immistaging_table.depdate.isNull(), 0)\
         .otherwise(immistaging_table.depdate))                
@@ -53,35 +76,47 @@ def process_immigration_data(spark, input_data, output_data):
         .datetime(1960,1,1)).strftime('%Y-%m-%d'))
 
     #convert arrival sas date to timestamp format 'YYYY-MM-DD'
-    immistaging_table = immistaging_table.withColumn("arrivaldate", \
-        (get_date(immistaging_table.arrdate))\
-        .cast(TimestampType()))              
+    immistaging_table = immistaging_table.\
+    withColumn("arrivaldate", (get_date(immistaging_table.arrdate)))             
 
     #convert depdate sas date timestamp format 'YYYY-MM-DD'
     immistaging_table = immistaging_table.withColumn('departuredate',\
-        F.when(immistaging_table.depdate == 0.0, 0)\
+        F.when(immistaging_table.depdate == 0, 0)\
         .otherwise(get_date(immistaging_table.depdate))\
         .cast(TimestampType()))                            
 
     #extract date data from staging table
     date_table = immistaging_table.select("passid", "arrivaldate", 
-        month('arrivaldate').alias('month'), year('arrivaldate').alias('year'), 
-        dayofmonth('arrivaldate').alias('day'), weekofyear('arrivaldate').alias('week'), 
+        month('arrivaldate').alias('month'), year('arrivaldate')\
+        .alias('year'), 
+        dayofmonth('arrivaldate').alias('day'), \
+        weekofyear('arrivaldate').alias('week'), 
         dayofweek('arrivaldate').alias('dayofweek'))
-
-    #extract passenger data from staging table
-    passenger_table = immistaging_table\
+    
+    #write date table to parquet file 
+    
+    date_table.write.partitionBy('year', 'month')\
+                    .parquet(output_data+'date_table.parquet')
+    
+    # extract passenger data from staging table and convert 
+    # data types to correct data types to get joins right
+    immigration_table = immistaging_table\
         .select("passid", "cicid", "i94yr", "i94mon",\
         "i94cit", "i94port", "i94mode", "i94addr",\
         "i94bir", "i94visa", "gender", \
         "arrivaldate", "departuredate")\
-        .withColumn("year", df["i94yr"].cast(IntegerType()))\
-        .withColumn("month", df["i94mon"].cast(IntegerType()))\
-        .withColumn("cic_id", df["cicid"].cast(LongType()))\
-        .withColumn("res_code", df["i94cit"].cast(IntegerType()))\
-        .withColumn("port_code", df["i94port"].cast(IntegerType()))\
-        .withColumn("mode_code", df["i94mode"].cast(IntegerType()))\
-        .withColumn("addr_code", df["i94addr"].cast(IntegerType()))\
+        .withColumn("year", immistaging_table["i94yr"]\
+            .cast(IntegerType()))\
+        .withColumn("month", immistaging_table["i94mon"]\
+            .cast(IntegerType()))\
+        .withColumn("cic_id", immistaging_table["cicid"]\
+            .cast(LongType()))\
+        .withColumn("res_code", immistaging_table["i94cit"]\
+            .cast(IntegerType()))\
+        .withColumn("port_code", immistaging_table["i94port"])\
+        .withColumn("mode_code", immistaging_table["i94mode"]\
+            .cast(IntegerType()))\
+        .withColumn("addr_code", immistaging_table["i94addr"])\
         .withColumnRenamed("i94bir", "age")\
         .withColumnRenamed("i94visa", "visa_code")\
         .drop("i94yr")\
@@ -93,24 +128,29 @@ def process_immigration_data(spark, input_data, output_data):
         .drop("i94addr")                       
 
     # update mode and visa code with values
-    passenger_table = immistaging_table\
+    immigration_table = immigration_table\
         .select("passid", "cic_id", "year", "month",\
         "res_code", "port_code", "mode_code", "addr_code",\
         "age", "visa_code", "gender", \
         "arrivaldate", "departuredate")\
-        .withColumn("mode", F.when(passenger_table\
+        .withColumn("mode", F.when(immigration_table\
         .mode_code == 1, 'Air')
-        .when(passenger_table.mode_code == 2, "Sea")\
-        .when(passenger_table.mode_code == 3, "Land")\
-        .when(passenger_table.mode_code == 9, 'Notreported')\
+        .when(immigration_table.mode_code == 2, "Sea")\
+        .when(immigration_table.mode_code == 3, "Land")\
+        .when(immigration_table.mode_code == 9, 'Notreported')\
         .otherwise(None))\
-        .withColumn("visa", F.when(passenger_table\
+        .withColumn("visa", F.when(immigration_table\
         .visa_code == 1, 'Business')\
-        .when(passenger_table.visa_code == 2, "Pleasure")\
-        .when(passenger_table.visa_code == 3, "Student").otherwise(None))
-
-   
-
+        .when(immigration_table.visa_code == 2, "Pleasure")\
+        .when(immigration_table.visa_code == 3, "Student")\
+        .otherwise(None))
+    
+       
+    #write passenger table to parquet
+    immigration_table.write.partitionBy('year', 'month')\
+        .parquet(output_data+'immigration_table.parquet') 
+    
+    
 def process_code_data(spark, code_data, output_data):
     """
     This fnction processes code files data
@@ -124,9 +164,18 @@ def process_code_data(spark, code_data, output_data):
 
     #Create port code table
     port_table = df.select("value", "i94prtl")\
-        .withColumn("port_code", df["value"].cast(IntegerType()))\
+        .withColumnRenamed("value", "port_code")\
         .withColumnRenamed("i94prtl", "port_city")
-
+    
+    port_table = port_table.withColumn\
+        ('city', \
+        split(port_table['port_city'], ',')[0])\
+        .withColumn('state', \
+        split(port_table['port_city'], ',')[1])
+    
+    #write port table to parquet file
+    port_table.write.parquet(output_data+'port_table.parquet')
+    
     #set input path for address city codes
     input_data = code_data + "I94ADDR.txt"
 
@@ -135,30 +184,53 @@ def process_code_data(spark, code_data, output_data):
         
     #Create address city code table
     addr_table = df.select("value", "i94addrl")\
-                    .withColumn("addr_code", df["value"]\
-                    .cast(IntegerType()))\
-                    .withColumnRenamed("i94addrl", "addr_city")
+        .withColumnRenamed("value", "addr_code")\
+        .withColumnRenamed("i94addrl", "addr_city")
+    
+    #write address table to parquet file
+    addr_table.write.parquet(output_data+'addr_table.parquet')
 
     #set input path for resident country codes
     input_data = code_data + "I94RES.txt"
-
+       
     #Load address city codes data into dataframe
     df=spark.read.csv(input_data, header=True, sep="=")
 
     #Create resident country code table
     rescountry_table = df.select("value", "i94cntyl")\
-                .withColumnRenamed("res_code", df["value"]\
-                .cast(IntegerType()))\
-                .withColumnRenamed("i94cntyl", "res_country")
+        .withColumn("res_code", df["value"]\
+        .cast(IntegerType()))\
+        .withColumnRenamed("i94cntyl", "res_country")\
+        .drop("value")
+    
+    #Write resident country table to parquet file
+    rescountry_table.write.\
+        parquet(output_data+'rescountry_table.parquet')
+    
+    #set input path for airport data
+    input_data = code_data + "airport-codes_csv.csv"
+    
+    #Create airport table for country US
+    df=spark.read.csv(input_data, header=True)
+    airport_table = df.select("name", "iso_country", "iso_region", \
+        "municipality").filter(df.iso_country == 'US')
 
+    #split region to separate state code and country
+    airport_table = airport_table\
+        .withColumn('state', split(df.iso_region, '-')[1])
+    
+    #write airport table to parquet file
+    airport_table.write.partitionBy('iso_country')\
+        .parquet(output_data+'airport_table.parquet')
+    
 def main():
     """
     This is main function of this module
     """
     spark = create_spark_session()
-    input_data  = "/Users/ISSAC/Documents/Projects/DEND/CapstoneProject/data/"
-    code_data   = "/Users/ISSAC/Documents/Projects/DEND/CapstoneProject/code/"
-    output_data = "/Users/ISSAC/Documents/Projects/DEND/CapstoneProject/outdata/"
+    input_data  = "s3a://immi-data/data/"
+    code_data   = "s3a://immi-data/code/"
+    output_data = "s3a://immi-data/data/outdata/"
     
     process_immigration_data(spark, input_data, output_data)
     process_code_data(spark, code_data, output_data)
